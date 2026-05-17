0df5cff6c489965779f341f4991659bdbe18fa34
mahi4203 <starkhoward425@gmail.com>
2026-05-17 10:25:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from datetime import date
 import os
 
 st.set_page_config(page_title="News Agent", page_icon="📰", layout="centered")
-st.title("📰 AI News Agent")
+st.title("Truthfeed AI")
 st.caption("Real-time verified news + background knowledge")
 
 GROQ_API_KEY   = st.secrets["GROQ_API_KEY"]
